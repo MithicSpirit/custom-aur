ad11725e0c9f65221b0001212874041372d7b6ae
MithicSpirit <rpc01234@gmail.com>
2021-12-07 18:30:24 -0500

    ttf-iosevka-mithic -> 11.2.0-4
    - Update commit
    - Get number of build jobs from MAKEFLAGS envvar, or defaults to 1
      (proper fix for OOM issue*)
    - Fix indentation
    
    *Don't set $MAKEFLAGS -j/--jobs too close to the number of free GBs of
    RAM you have, lest the build (or maybe entire system) crashes.

---
+++ ttf-iosevka-mithic/get-makeflags.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+
+const makeflags = process.env.MAKEFLAGS;
+if (makeflags === undefined) process.exit(0);
+
+matches = makeflags.matchAll(/-+j(?:obs)?(?: *|=)(?<threads>\d+)/gm);
+let last_match;
+let next_match;
+do {
+	last_match = next_match;
+	next_match = matches.next();
+} while (!next_match.done);
+
+if (
+	last_match === undefined ||
+	(val = last_match.value) === undefined ||
+	(gps = val.groups) === undefined ||
+	(thr = gps.threads) === undefined
+)
+    process.exit(0);
+
+process.stdout.write("--jCmd=");
+process.stdout.write(thr);
